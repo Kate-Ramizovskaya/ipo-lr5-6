49746ac5f5c1f5e58097d04f1933fdd846aa80f2
Kate Ramizovskaya <kr978407@gmail.com>
2024-10-22 17:15:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-with open ('text.txt', 'r', encoding='utf-8') as infile, open('output.txt', 'w', encoding='utf-8') as outfile:
-    data=infile.readlines()
-    i=0
-    for str in data:
-        if len(data[i])>5:
-            outfile.write(data[i])
-        i+=1
+with open ('text.txt', 'r', encoding='utf-8') as infile, open('output.txt', 'w', encoding='utf-8') as outfile:#открытие файлов
+    data=infile.readlines()#перекидывание первого файла в список
+    i=0#инициализация индекса
+    for str in data:#цикл для перебора 
+        if len(data[i])>5:#for для исключение не подходящих
+            outfile.write(data[i])#переписывание подходящих в новый файл
+        i+=1#увеличение индекса для дальнейшей работы
